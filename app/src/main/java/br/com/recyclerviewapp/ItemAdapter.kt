e46ad6a3341720a8e544fdaebea57d5741d0421c
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-06-20 20:17:48 -0300

    Update Start

---
+++ app/src/main/java/br/com/recyclerviewapp/ItemAdapter.kt
@@ -0,0 +1,52 @@
+package br.com.recyclerviewapp
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(
+    private val itemList: List<Item>,
+    private val listener: OnItemClickListener
+    ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+    View.OnClickListener{
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+        val textView1: TextView = itemView.findViewById(R.id.textView1)
+        val textView2: TextView = itemView.findViewById(R.id.textView2)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position: Int = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item,
+            parent, false)
+        return ItemViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val currentItem = itemList[position]
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.setText(currentItem.text1)
+        holder.textView2.setText(currentItem.text2)
+    }
+
+    override fun getItemCount() = itemList.size
+
+
+}
+
